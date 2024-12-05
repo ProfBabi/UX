758cb56774b7e6f0e58b6435cbe77b683c82f4ff
Babi Almeida <barbara_almeida12@yahoo.com.br>
2024-12-05 17:01:21 -0300

    Add files via upload

---
+++ Qualidade de Software/teste.js
@@ -0,0 +1,7 @@
+document.getElementById('aumentar').addEventListener('click', function() { //EventListener ocorre quando se precisa ouvir um evento, exemplo quando se clica no mouse, é um evento escutado.
+  document.body.style.fontSize = '18px';
+});
+
+document.getElementById('diminuir').addEventListener('click', function() {
+  document.body.style.fontSize = '14px';
+});
